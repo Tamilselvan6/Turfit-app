5132b1fdf05be56c66cb20b08a7bd9f52faf4848
Tamilselvan V <126091412+Tamilselvan6@users.noreply.github.com>
2025-04-15 09:06:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 // CORS Configuration
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://turfit-application.onrender.com"];
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -83,7 +83,7 @@ const connectDB = async () => {
 connectDB();
 
 // Start server with both HTTP and WebSocket
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 ;
 server.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
